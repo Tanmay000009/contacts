840099888432a56229333e8b8c7283a7e1aeccc6
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-05 10:05:20 +0530

    refactor: user routes refactor

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,80 +1,27 @@
-const { response } = require("express");
 const express = require("express");
-
 const router = express.Router();
 
 /** load the service */
-const { UserController } = require("../app/controllers/user.controller");
+const UserController = require("../app/controllers/user.controller");
 const {
   userValidationRules,
   validate,
 } = require("../app/validators/user.validator");
 
 /** to list all users */
-router.get("/", async (req, res) => {
-  await UserController.getAllUsers()
-  .then( userList => {
-    return res.json(userList);
-  })
-  .catch( e => {
-    return res.status(400).json({
-      errors: e
-    });
-  })
-});
+router.get("/", UserController.getAllUsers);
 
 /** to list specific user */
-router.get("/:id", async (req, res) => {
-  await UserController.getOneUser(req.params.id)
-  .then( user => {
-    return res.json(user);
-  })
-  .catch( e => {
-    return res.status(400).json({
-      errors: e
-    });
-  })
-});
+router.get("/:id", UserController.getOneUser);
 
 /** to register a user */
-router.post("/", userValidationRules(), validate, async (req, res) => {
-  await UserController.register(req.body)
-  .then( response => {
-    return res.send(response);
-  })
-  .catch(e => {
-    return res.status(400).json({
-      errors: e
-    });
-  })
-
-});
+router.post("/",userValidationRules(),validate ,UserController.register);
 
 /** to update a user */
-router.put("/", async (req, res) => {
-  await UserController.register(req.body)
-  .then( user => {
-    return res.json(user);
-  })
-  .catch( e => {
-    return res.status(400).json({
-      errors: e
-    });
-  })
-});
+router.put("/",userValidationRules(),validate, UserController.register);
 
 /** to delete a user */
-router.delete("/:id", async (req, res) => {
-  await UserController.delete(req.params.id)
-  .then( user => {
-    return res.json(user);
-  })
-  .catch( e => {
-    return res.status(400).json({
-      errors: e
-    });
-  })
-});
+router.delete("/:id", UserController.delete);
 
 /** export the routes to be binded to application */
 module.exports = router;
