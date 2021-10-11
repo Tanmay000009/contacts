fb143bef0e931e12f9056d24e84588d8dcf8306d
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-11 17:00:25 +0530

    feat: contacts controller setup

---
+++ app/controllers/contacts.controller.js
@@ -0,0 +1,61 @@
+/** load required packages */
+const { json } = require("express");
+
+/** load peer modules and services */
+const { logger } = require("../services/logger");
+const { ContactService } = require("../services/contacts.service");
+const apiResponse = require("../helpers/apiResponse");
+
+module.exports.getAllContacts = async (req,res) => {
+  try {
+    logger.info("[Contact]: listing all Contacts");
+    const ContactList = await ContactService.findAllContacts();
+    return apiResponse.successResponseWithData(res, "Operation success", ContactList);
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e);
+  }
+};
+
+module.exports.getOneContact = async (req,res) => {
+  try {
+    logger.info("[Contact]: finding the Contact");
+    const Contact = await ContactService.findContact(req.params.id);
+    return apiResponse.successResponseWithData(res, "Operation success", Contact);
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e);
+  }
+};
+
+module.exports.register = async (req,res) => {
+  try {
+    logger.info("[Contact]: registering the Contact");
+    const Contact = await ContactService.registerContact(req.body);
+    return apiResponse.successResponseWithData(res, "Operation success", Contact);
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e.message);
+  }
+};
+
+module.exports.update = async (req,res) => {
+  try {
+    logger.info("[Contact]: updating the Contact");
+    const Contact = await ContactService.updateContact(req.body.id,req.body);
+    return apiResponse.successResponseWithData(res, "Operation success", Contact);
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e);
+  }
+};
+
+module.exports.delete = async (req,res) => {
+  try {
+    logger.info("[Contact]: deleting the Contact");
+    return apiResponse.successResponse(res,"Contact delete Success.")
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e);
+  }
+};
