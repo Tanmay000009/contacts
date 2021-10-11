e4182c4e4ce3a0486b6e5c2f8176d30dd602f4e2
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-11 17:48:35 +0530

    refactor: send error.message instead of error

--- app/controllers/contacts.controller.js
+++ app/controllers/contacts.controller.js
@@ -6,56 +6,72 @@ const { logger } = require("../services/logger");
 const { ContactService } = require("../services/contacts.service");
 const apiResponse = require("../helpers/apiResponse");
 
-module.exports.getAllContacts = async (req,res) => {
+module.exports.getAllContacts = async (req, res) => {
   try {
     logger.info("[Contact]: listing all Contacts");
     const ContactList = await ContactService.findAllContacts();
-    return apiResponse.successResponseWithData(res, "Operation success", ContactList);
-  } catch (e) {
-    logger.error("Error: " + e);
-    apiResponse.ErrorResponse(res, e);
-  }
-};
-
-module.exports.getOneContact = async (req,res) => {
-  try {
-    logger.info("[Contact]: finding the Contact");
-    const contact = await ContactService.findContact(req.params.id);
-    return apiResponse.successResponseWithData(res, "Operation success", contact);
-  } catch (e) {
-    logger.error("Error: " + e);
-    apiResponse.ErrorResponse(res, e);
-  }
-};
-
-module.exports.register = async (req,res) => {
-  try {
-    logger.info("[Contact]: registering the Contact");
-    const contact = await ContactService.registerContact(req.body,req.user.id);
-    return apiResponse.successResponseWithData(res, "Operation success", contact);
+    return apiResponse.successResponseWithData(
+      res,
+      "Operation success",
+      ContactList
+    );
   } catch (e) {
     logger.error("Error: " + e);
     apiResponse.ErrorResponse(res, e.message);
   }
 };
 
-module.exports.update = async (req,res) => {
+module.exports.getOneContact = async (req, res) => {
   try {
-    logger.info("[Contact]: updating the Contact");
-    const contact = await ContactService.updateContact(req.body.id,req.body);
-    return apiResponse.successResponseWithData(res, "Operation success", contact);
+    logger.info("[Contact]: finding the Contact");
+    const contact = await ContactService.findContact(req.params.id);
+    return apiResponse.successResponseWithData(
+      res,
+      "Operation success",
+      contact
+    );
   } catch (e) {
     logger.error("Error: " + e);
-    apiResponse.ErrorResponse(res, e);
+    apiResponse.ErrorResponse(res, e.message);
   }
 };
 
-module.exports.delete = async (req,res) => {
+module.exports.register = async (req, res) => {
   try {
-    logger.info("[Contact]: deleting the Contact");
-    return apiResponse.successResponse(res,"Contact delete Success.")
+    logger.info("[Contact]: registering the Contact");
+    const contact = await ContactService.registerContact(req.body, req.user.id);
+    return apiResponse.successResponseWithData(
+      res,
+      "Operation success",
+      contact
+    );
   } catch (e) {
     logger.error("Error: " + e);
-    apiResponse.ErrorResponse(res, e);
+    apiResponse.ErrorResponse(res, e.message);
+  }
+};
+
+module.exports.update = async (req, res) => {
+  try {
+    logger.info("[Contact]: updating the Contact");
+    const contact = await ContactService.updateContact(req.params.id, req.body);
+    return apiResponse.successResponseWithData(
+      res,
+      "Operation success",
+      contact
+    );
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e.message);
+  }
+};
+
+module.exports.delete = async (req, res) => {
+  try {
+    logger.info("[Contact]: deleting the Contact");
+    return apiResponse.successResponse(res, "Contact delete Success.");
+  } catch (e) {
+    logger.error("Error: " + e);
+    apiResponse.ErrorResponse(res, e.message);
   }
 };
