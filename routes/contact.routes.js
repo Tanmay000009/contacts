8c6649534f0faa4c12d9561e3cb512205dc1d89c
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-11 16:57:13 +0530

    feat: contact route setup

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,6 +1,23 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+
+/** load the service */
+const ContactsController = require("../app/controllers/contacts.controller");
+
+/** to list all Contacts */
+router.get("/", ContactsController.getAllContacts);
+
+/** to list specific Contacts */
+router.get("/:id", ContactsController.getOneContacts);
+
+/** to register a Contacts */
+router.post("/" ,ContactsController.register);
+
+/** to update a Contacts */
+router.put("/",ContactsController.update);
+
+/** to delete a Contacts */
+router.delete("/:id", ContactsController.delete);
 
 /** export the routes to be binded to application */
 module.exports = router;
