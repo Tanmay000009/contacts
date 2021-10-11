07db97efa54de4b07f3ac6affce2c5a928cf1a44
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-11 17:23:02 +0530

    refactor: update register function

--- app/controllers/contacts.controller.js
+++ app/controllers/contacts.controller.js
@@ -20,8 +20,8 @@ module.exports.getAllContacts = async (req,res) => {
 module.exports.getOneContact = async (req,res) => {
   try {
     logger.info("[Contact]: finding the Contact");
-    const Contact = await ContactService.findContact(req.params.id);
-    return apiResponse.successResponseWithData(res, "Operation success", Contact);
+    const contact = await ContactService.findContact(req.params.id);
+    return apiResponse.successResponseWithData(res, "Operation success", contact);
   } catch (e) {
     logger.error("Error: " + e);
     apiResponse.ErrorResponse(res, e);
@@ -31,8 +31,8 @@ module.exports.getOneContact = async (req,res) => {
 module.exports.register = async (req,res) => {
   try {
     logger.info("[Contact]: registering the Contact");
-    const Contact = await ContactService.registerContact(req.body);
-    return apiResponse.successResponseWithData(res, "Operation success", Contact);
+    const contact = await ContactService.registerContact(req.body,req.user.id);
+    return apiResponse.successResponseWithData(res, "Operation success", contact);
   } catch (e) {
     logger.error("Error: " + e);
     apiResponse.ErrorResponse(res, e.message);
@@ -42,8 +42,8 @@ module.exports.register = async (req,res) => {
 module.exports.update = async (req,res) => {
   try {
     logger.info("[Contact]: updating the Contact");
-    const Contact = await ContactService.updateContact(req.body.id,req.body);
-    return apiResponse.successResponseWithData(res, "Operation success", Contact);
+    const contact = await ContactService.updateContact(req.body.id,req.body);
+    return apiResponse.successResponseWithData(res, "Operation success", contact);
   } catch (e) {
     logger.error("Error: " + e);
     apiResponse.ErrorResponse(res, e);
