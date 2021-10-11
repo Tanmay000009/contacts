6ca0c4d83e1f5db2a0c60c61381e17e8bccfff53
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-11 16:49:48 +0530

    feat: contact schema setup

---
+++ app/models/contact.schema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+/**
+ * Define the structure of User document here
+ */
+
+const ContactSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Contact", ContactSchema);
