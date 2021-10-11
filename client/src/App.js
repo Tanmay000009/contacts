dd2f2c073ecdb368096bddbb96cecd62b52262e6
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-12 01:25:28 +0530

    feat: basic contact state setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Fragment } from "react";
-import Home from './components/pages/Home';
-import About from './components/pages/About';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 
+import ContactState from "./context/contact/ContactState";
+
 const App = () => {
   return (
-    <Router>
-      <Fragment className="App">
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route exact path="/about" component={ About } />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment className="App">
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 };
 
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,4 +1,75 @@
-import React, {useReducer} from 'react';
-import uuid from 'uuid';
-import ContactContext from './contactContext';
-import contactReducer from './contactReducer';
+import React, { useReducer } from "react";
+import uuid from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const intialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "a",
+        email: "a@b.com",
+        phone: "1",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "b",
+        email: "b@b.com",
+        phone: "2",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "c",
+        email: "c@b.com",
+        phone: "3",
+        type: "professional",
+      },
+    ],
+  };
+
+  const [state,dispatch] = useReducer(contactReducer,intialState);
+
+  // Add Contact
+
+
+  // Delete Contact
+
+
+  // Set Current Contact
+
+
+  // Clear Current Contact
+
+
+  // Update Contact
+
+
+  // Filter Contacts
+
+
+  // Clear Filter
+
+
+  return (
+      <ContactContext.Provider
+        value = {{
+            contacts: state.contacts
+        }}>
+          {props.children}
+      </ContactContext.Provider>
+  )
+};
+
+export default ContactState;
