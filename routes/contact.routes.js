638dab1bc29de1d0ee894eaaada42f7eb1e70b40
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-11 17:47:45 +0530

    bug: add param in put request

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,25 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../app/middleware/auth');
+const auth = require("../app/middleware/auth");
 
 /** load the service */
 const ContactsController = require("../app/controllers/contacts.controller");
 const {
-    contactValidationRules,
-    validate,
-  } = require("../app/validators/contact.validator");
+  contactValidationRules,
+  validate,
+} = require("../app/validators/contact.validator");
 
 /** to list all Contacts */
-router.get("/",auth ,ContactsController.getAllContacts);
+router.get("/", auth, ContactsController.getAllContacts);
 
 /** to list specific Contacts */
 router.get("/:id", auth, ContactsController.getOneContact);
 
 /** to register a Contacts */
-router.post("/" , auth,contactValidationRules(),validate,ContactsController.register);
+router.post(
+  "/",
+  auth,
+  contactValidationRules(),
+  validate,
+  ContactsController.register
+);
 
 /** to update a Contacts */
-router.put("/", auth,contactValidationRules(),validate ,ContactsController.update);
+router.put(
+  "/:id",
+  auth,
+  contactValidationRules(),
+  validate,
+  ContactsController.update
+);
 
 /** to delete a Contacts */
 router.delete("/:id", auth, ContactsController.delete);
