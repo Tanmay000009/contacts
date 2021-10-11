f08e839a50aaa81a3f2ee485049c359a45f4ebe2
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-12 01:39:06 +0530

    feat: render contact

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,19 @@
+import React, { Fragment, useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+const ContactItem = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+  return <div>
+      <Fragment>
+          { contacts.map(contact => (
+              <h3>
+                  {contact.name}
+              </h3>
+          ))}
+      </Fragment>
+  </div>;
+};
+
+export default ContactItem;
