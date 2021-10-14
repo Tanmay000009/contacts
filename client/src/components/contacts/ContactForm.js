91e79cdf5439b29e974ea4fa3cd579be46c56810
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-14 15:43:34 +0530

    feat: add clear method

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -4,7 +4,7 @@ import ContactContext from '../../context/contact/contactContext';
 const ContactForm = () => {
     const contactContext = useContext(ContactContext);
 
-    const {addContact, current} = contactContext;
+    const {addContact, current, clearCurrent} = contactContext;
 
     useEffect( () => {
         if (current != null) {
@@ -41,6 +41,10 @@ const ContactForm = () => {
         })
     }
 
+    const clearAll = () => {
+        clearCurrent();
+    }
+
     return (
         <form onSubmit={onSubmit}>
             <h2 className="text=primary">{current ? 'Update Contact' : 'Add Contact' }</h2>
@@ -53,6 +57,9 @@ const ContactForm = () => {
             <div>
                 <input type="submit" value={current ? 'Update Contact' : 'Add Contact' } className="btn btn-primary btn-block" />
             </div>
+            { current && <div>
+                <button className="btn btn-light btn-block" onClick={clearAll} > Clear</button>    
+            </div>}
         </form>
     )
 }
