365151c15d7feea3caea5cdd8708206ecd800487
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-14 15:57:41 +0530

    feat: basic setup for filter contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = (props) => {
         type: "professional",
       },
     ],
-    current: null
+    current: null,
+    filtered: null,
   };
 
   const [state,dispatch] = useReducer(contactReducer,intialState);
@@ -69,21 +70,28 @@ const ContactState = (props) => {
   }
 
   // Filter Contacts
-
+  const filterContact = text => {
+    dispatch({type: FILTER_CONTACTS, payload: text});
+  }
 
   // Clear Filter
-
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER});
+  }
 
   return (
       <ContactContext.Provider
         value = {{
             contacts: state.contacts,
             current: state.current,
+            filtered: state.filtered,
             addContact,
             deleteContact,
             setCurrent,
             clearCurrent,
-            updateContact
+            updateContact,
+            filterContact,
+            clearFilter
         }}>
           {props.children}
       </ContactContext.Provider>
