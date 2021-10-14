47c90c68a83bad078857a49b7c33be49d38788bd
Tanmay000009 <tanmayvyas09@gmail.com>
2021-10-14 16:07:53 +0530

    feat: contact filter setup

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,17 +1,25 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactFilter = () => {
 
     const contactContext = useContext(ContactContext);
 
+    const {filterContacts, clearFilter,filtered} = contactContext;
+
     const text = useRef('');
+
+    useEffect( () => {
+        if(filtered == null) {
+            text.current.value = '';
+        }
+    })
 
     const onChange = e => {
        if (text.current.value !== '') {
-           contactContext.filterContacts(e.target.value);
+           filterContacts(e.target.value);
        } else {
-           contactContext.clearFilter();
+           clearFilter();
        }
     }
 
